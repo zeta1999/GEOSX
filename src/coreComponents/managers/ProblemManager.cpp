b1337c4ec413729fd8f79157bcb8794c1c65f1d4
Benjamin Curtice Corbett <corbett5@llnl.gov>
2020-07-30 17:01:17 -0700

    No longer cd-ing to the output directory, vtk output now in the wrong spot but all tests pass rentrant.

--- src/coreComponents/managers/ProblemManager.cpp
+++ src/coreComponents/managers/ProblemManager.cpp
@@ -195,15 +195,6 @@ void ProblemManager::ParseCommandLineInput()
     string notUsed;
     splitPath( inputFileName, xmlFolder, notUsed );
     Path::pathPrefix() = xmlFolder;
-
-    if( outputDirectory != "." )
-    {
-      mkdir( outputDirectory.data(), 0755 );
-      if( chdir( outputDirectory.data()) != 0 )
-      {
-        GEOSX_ERROR( "Could not change to the output directory: " + outputDirectory );
-      }
-    }
   }
 
   if( opts.suppressMoveLogging )
